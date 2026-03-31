0ef1dbcc4a91975e2ead6cb040a5a9aed9605b23
weew12 <weew12@2528566339@qq.com>
2026-03-31 20:51:50 +0800

    注释 version

--- gateway/version/version.go
+++ gateway/version/version.go
@@ -1,20 +1,21 @@
+// Package version 管理应用版本信息
 package version
 
 var (
-	// Version release version of the provider
+	// Version 正式发布版本号
 	Version string
 
-	// GitCommitSHA is the Git SHA of the latest tag/release
+	// GitCommitSHA 最新提交的Git SHA
 	GitCommitSHA string
 
-	// GitCommitMessage as read from the latest tag/release
+	// GitCommitMessage Git提交信息
 	GitCommitMessage = "See GitHub for latest changes"
 
-	// DevVersion string for the development version
+	// DevVersion 开发版本标识
 	DevVersion = "dev"
 )
 
-// BuildVersion returns current version of the provider
+// BuildVersion 返回当前版本，未设置正式版本则返回开发版本
 func BuildVersion() string {
 	if len(Version) == 0 {
 		return DevVersion
